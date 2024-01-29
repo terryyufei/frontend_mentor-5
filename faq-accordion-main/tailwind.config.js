e4cb425000bd9a4d6ded87ef969bcf55dc6d8068
Terry Wambui <shikuwambui@gmail.com>
2024-01-29 13:48:59 +0300

    desktop and mobile version

--- faq-accordion-main/tailwind.config.js
+++ faq-accordion-main/tailwind.config.js
@@ -7,13 +7,17 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "font-body": "'Work Sans', sans-serif",
+        "body": "'Work Sans', sans-serif",
       },
       colors: {
         "lightPink": "hsl(275, 100%, 97%)",
         "grayishPurple": "hsl(292, 16%, 49%)",
         "darkPurple": "hsl(292, 42%, 14%)",
       },
+      backgroundImage: {
+        'desktop': "url('./images/pattern-desktop.svg')",
+        'mobile': "url('./images/pattern-mobile.svg')",
+      }
     },
   },
   plugins: [],
